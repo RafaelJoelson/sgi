152cbcd74c6f92b373e6653859ceee746bcf81e0
Joelson <144927205+RafaelJoelson@users.noreply.github.com>
2025-07-22 18:03:44 -0300

    Cria páginas de erros web service
    - páginas dos 400,401,403,404,500,503.

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
         <p><?php echo SITE_DESCRIPTION; ?></p>
         <p>Versão Beta <?php echo ASSET_VERSION; ?></p>
     </footer>
-    <a href="<?= BASE_URL ?>/pages/utils/suporte.php" class="suporte-btn">
+    <a href="<?= BASE_URL ?>/pages/utils/suporte.html" class="suporte-btn">
         <img src="<?= BASE_URL ?>/img/suporte.png" alt="Suporte" class="icone">
         <span class="texto">Precisa de ajuda?</span>
     </a>
